2f9b9b6914e5acb83cf42854cc81e23ad9473663
unknown <sup.makulin@mail.ru>
2024-01-16 23:48:40 +0300

    let start create second spring project on hw3

---
+++ sem3HomeTask/src/main/java/com/example/sem3HomeTask/services/RegistrationService.java
@@ -0,0 +1,29 @@
+package com.example.sem3HomeTask.services;
+
+import com.example.sem3HomeTask.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+
+    public DataProcessingService getDataProcessingService() {
+        return dataProcessingService;
+    }
+
+    @Autowired
+    private DataProcessingService dataProcessingService;
+
+    //Поля UserService, NotificationService
+    @Autowired
+    private UserService userService;
+
+//    @Autowired
+//    private NotificationService notificationService;
+
+    // Регистрация нового пользователя (создание и добавление в общий список)
+    public void processRegistration(String name, int age, String email){
+        User user = userService.createUser(name, age, email);
+        dataProcessingService.addUserToList(user);
+    }
+}
