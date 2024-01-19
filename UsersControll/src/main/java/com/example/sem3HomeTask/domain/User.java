94ffd6ae81bb47e195f8b0d0ebdc4edcf5e6e000
unknown <sup.makulin@mail.ru>
2024-01-19 21:43:16 +0300

    lombok injected

--- UsersControll/src/main/java/com/example/sem3HomeTask/domain/User.java
+++ UsersControll/src/main/java/com/example/sem3HomeTask/domain/User.java
@@ -1,45 +1,11 @@
 package com.example.sem3HomeTask.domain;
 
+import lombok.Data;
+
+@Data
 public class User {
     private int id;
     private String firstName;
     private int age;
     private String email;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return getFirstName() + ", age: " + getAge() + ", email: " + getEmail();
-    }
 }
