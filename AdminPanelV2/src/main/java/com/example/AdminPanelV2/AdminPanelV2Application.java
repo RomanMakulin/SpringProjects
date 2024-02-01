206541120673e8e889df15a2c3d7b1d58381deb8
unknown <sup.makulin@mail.ru>
2024-02-01 18:22:55 +0300

    create project dependences

---
+++ AdminPanelV2/src/main/java/com/example/AdminPanelV2/AdminPanelV2Application.java
@@ -0,0 +1,13 @@
+package com.example.AdminPanelV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPanelV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPanelV2Application.class, args);
+	}
+
+}
---
+++ AdminPanelV2/src/test/java/com/example/AdminPanelV2/AdminPanelV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdminPanelV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPanelV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
