70414e144b6b58dafe6f62854448fffa34a7f164
unknown <sup.makulin@mail.ru>
2024-02-26 22:13:16 +0300

    реализовал логику бронирования дат при записи на сессию

--- AnnPsychology/src/main/java/com/AnnPsychology/AnnPsychology/services/UserPageService/CustomUserDetailsService.java
+++ AnnPsychology/src/main/java/com/AnnPsychology/AnnPsychology/services/UserPageService/CustomUserDetailsService.java
@@ -6,6 +6,7 @@ import com.AnnPsychology.AnnPsychology.models.SessionDate;
 import com.AnnPsychology.AnnPsychology.models.User;
 import com.AnnPsychology.AnnPsychology.models.enums.SessionStatus;
 import com.AnnPsychology.AnnPsychology.models.enums.UserRole;
+import com.AnnPsychology.AnnPsychology.repository.DateRepository;
 import com.AnnPsychology.AnnPsychology.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,14 +19,18 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final DateRepository dateRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = userRepository.getByUsername(username);
@@ -33,7 +38,7 @@ public class CustomUserDetailsService implements UserDetailsService {
                 orElseThrow(() -> new UsernameNotFoundException(username + " not found!"));
     }
 
-    public User getById(Long id){
+    public User getById(Long id) {
         return userRepository.findById(id).orElseThrow();
     }
 
@@ -66,19 +71,40 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     public void signUpSession(Long id, LocalDate date, LocalTime time) {
-        User updUser = userRepository.findById(id).orElseThrow();
 
-        Session session = new Session();
-        session.setUser(updUser);
-        session.setSessionStatus(SessionStatus.SESSION_ACTIVE);
-        session.setSessionPrice(updUser.getPrice());
+        // Формируем список занятых дат
+        List<LocalDateTime> localDateTimeList = new ArrayList<>();
+        dateRepository.findAll().forEach(item -> {
+            localDateTimeList.add(item.getSessionDate());
+        });
 
-        SessionDate sessionDate = new SessionDate();
-        sessionDate.setSessionDate(LocalDateTime.of(date, time));
-        session.setSessionDate(sessionDate);
+        // 1. Дата сесси не должна быть занята
+        boolean validDateContain = localDateTimeList.contains(LocalDateTime.of(date, time));
 
-        updUser.getSessionList().add(session);
-        userRepository.save(updUser);
+        // 2. Сессия не должна пересекаться с другими
+        // 3. Между сессиями должен быть перерыв 30 мин
+        boolean validSessionBefore = localDateTimeList.contains(LocalDateTime.of(date, time.plusHours(1))) |
+                localDateTimeList.contains(LocalDateTime.of(date, time.plusMinutes(30)));
+
+        boolean validSessionAfter = localDateTimeList.contains(LocalDateTime.of(date, time.minusHours(1))) |
+                localDateTimeList.contains(LocalDateTime.of(date, time.minusMinutes(30)));
+
+        if (!validDateContain & !validSessionBefore & !validSessionAfter) {
+            User updUser = userRepository.findById(id).orElseThrow();
+
+            Session session = new Session();
+            session.setUser(updUser);
+            session.setSessionStatus(SessionStatus.SESSION_ACTIVE);
+            session.setSessionPrice(updUser.getPrice());
+
+            SessionDate sessionDate = new SessionDate();
+            sessionDate.setSessionDate(LocalDateTime.of(date, time));
+            session.setSessionDate(sessionDate);
+
+            updUser.getSessionList().add(session);
+            userRepository.save(updUser);
+        }
+
     }
 
 }
