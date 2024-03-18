e1f561cb6d114cd407890424480ddca8b530948b
unknown <sup.makulin@mail.ru>
2024-03-18 21:11:02 +0300

    доделал задачу с собеседования

---
+++ Test/src/main/java/org/example/Test.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Random;
+
+public class Test {
+
+    /*
+В массиве содержатся все числа от 1 до 100, причем одно из них встречается дважды.
+Напишите метод, принимающий указанный массив, который вернет дублирующийся элемент.
+*/
+
+    public static int[] initArray() {
+        int[] arr = new int[101];
+        int minNumber = 1;
+        int maxNumber = 100;
+        for (int nubmer = minNumber, pos = 0; nubmer <= maxNumber; nubmer++, pos++) {
+            arr[pos] = nubmer;
+        }
+        Random random = new Random();
+        int randomNumber = random.nextInt(maxNumber - minNumber + 1) + minNumber;
+        System.out.println("randomNumber is " + randomNumber);
+        arr[100] = randomNumber;
+        return arr;
+    }
+
+
+
+}
