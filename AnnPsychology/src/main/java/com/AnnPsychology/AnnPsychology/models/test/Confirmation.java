1487ce11098d6bbc7e54c92e29ced54a1bd86b26
WayWayz <32876995+RomanMakulin@users.noreply.github.com>
2024-03-25 08:25:39 +0300

    Update Confirmation.java

--- AnnPsychology/src/main/java/com/AnnPsychology/AnnPsychology/models/test/Confirmation.java
+++ AnnPsychology/src/main/java/com/AnnPsychology/AnnPsychology/models/test/Confirmation.java
@@ -7,4 +7,10 @@ public class Confirmation {
     private String type;
     private String confirmation_url;
     private String return_url;
+
+     public Confirmation(String type, String return_url) {
+        this.type = type;
+        this.return_url = return_url;
+    }
+    
 }
