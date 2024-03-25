d4904540143b9b58639596047f14ca882a9d6bd4
WayWayz <32876995+RomanMakulin@users.noreply.github.com>
2024-03-25 08:29:27 +0300

    Update PaymentForAPI.java

--- AnnPsychology/src/main/java/com/AnnPsychology/AnnPsychology/models/test/PaymentForAPI.java
+++ AnnPsychology/src/main/java/com/AnnPsychology/AnnPsychology/models/test/PaymentForAPI.java
@@ -9,9 +9,10 @@ public class PaymentForAPI {
     private Confirmation confirmation;
     private String description;
 
-    public void setConfirmation(String type, String return_url) {
-        this.confirmation = new Confirmation();
-        confirmation.setType(type);
-        confirmation.setReturn_url(return_url);
+    public PaymentForAPI(User user, String currency, String description) {
+        this.amount = new Amount(user.getPrice(), currency);
+        this.capture = true;
+        this.confirmation = new Confirmation("redirect", "https://ann-novikova.ru/user");
+        this.description = description;
     }
 }
