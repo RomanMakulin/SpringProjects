6c0fb385b2e53e162ef974c8f27d853f7d7124c6
WayWayz <32876995+RomanMakulin@users.noreply.github.com>
2024-03-25 08:34:32 +0300

    Update Payment.java

--- AnnPsychology/src/main/java/com/AnnPsychology/AnnPsychology/services/user/Payment.java
+++ AnnPsychology/src/main/java/com/AnnPsychology/AnnPsychology/services/user/Payment.java
@@ -13,30 +13,21 @@ import java.math.BigDecimal;
 
 public class Payment {
     public PaymentStatusForAPI pay(User user) throws JsonProcessingException {
+        
         RestTemplate restTemplate = new RestTemplate();
-
-        String url = "https://api.yookassa.ru/v3/payments";
-        PaymentForAPI payment = new PaymentForAPI();
-        payment.setAmount(new Amount(user.getPrice(), "RUB"));
-        payment.setCapture(true);
-        payment.setConfirmation("redirect", "https://ann-novikova.ru/shop-verification-5Pvg3Jkv09.txt");
-        payment.setDescription("Оплата сессии");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(payment);
-
         HttpHeaders headers = new HttpHeaders();
+        
+        PaymentForAPI payment = new PaymentForAPI(user, "RUB", "Оплата онлайн сессии");
+        String json = new ObjectMapper().writeValueAsString(payment);
+        String url = "https://api.yookassa.ru/v3/payments";
+
         headers.setBasicAuth("352122", "test_R9DnTTLp0AJ5mth_es0DTrnGgeQOQIH7320XtzesxnI");
         headers.set("Idempotence-Key", "17edd6235d"); // настроить генерацию ключа рандомно
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        HttpEntity<String> request =
-                new HttpEntity<>(json, headers);
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
+        ResponseEntity<PaymentStatusForAPI> response = restTemplate.exchange(url, HttpMethod.POST, request, PaymentStatusForAPI.class);
 
-        ResponseEntity<PaymentStatusForAPI> response =
-                restTemplate.exchange(url, HttpMethod.POST, request, PaymentStatusForAPI.class);
-
-        System.out.println(response.getBody().getConfirmation().getConfirmation_url());
         return response.getBody();
     }
 }
